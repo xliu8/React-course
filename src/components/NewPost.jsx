480c2c739147788b8769f639be7b3d0e79d99ebc
xliu8 <117231787+xliu8@users.noreply.github.com>
2024-01-10 15:55:42 -0600

    new update

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
+
 import classes from "./NewPost.module.css";
 
 function NewPost({ onCancel, onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
-
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  function bodyChangedHandler(event) {
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  function authorChangedHandler(event) {
+  function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
 
@@ -20,24 +20,25 @@ function NewPost({ onCancel, onAddPost }) {
       body: enteredBody,
       author: enteredAuthor,
     };
-    onCancel();
     onAddPost(postData);
+    onCancel();
   }
+
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={bodyChangedHandler} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={authorChangedHandler} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
           Cancel
         </button>
-        <button type="submit">Submit</button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -8,7 +8,24 @@ import classes from "./PostsList.module.css";
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -28,8 +45,8 @@ function PostsList({ isPosting, onStopPosting }) {
       )}
       {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
-          <h2>There are no post yet</h2>
-          <p>Start to add some</p>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
         </div>
       )}
     </>
